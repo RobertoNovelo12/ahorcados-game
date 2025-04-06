0d68eeb16d18a3e84de03990e05d8e479387a5de
RobertoNovelo12 <robertonovelo98@gmail.com>
2025-04-06 17:47:23 -0500

    ahora los filtros cambian con filter y no con src

--- js/theme-logic.js
+++ js/theme-logic.js
@@ -1,22 +1,16 @@
 export function toggleTheme() {
     const body = document.body;
-    const themeIcon = document.getElementById('themeIcon');
-  
     const isDark = body.classList.toggle('dark');
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    themeIcon.src = isDark ? 'public/sun-icon.svg' : 'public/moon-icon.svg';
   }
   
   export function applyStoredTheme() {
     const storedTheme = localStorage.getItem('theme');
     const body = document.body;
-    const themeIcon = document.getElementById('themeIcon');
   
     if (storedTheme === 'dark') {
       body.classList.add('dark');
-      themeIcon.src = 'public/sun-icon.svg';
     } else {
       body.classList.remove('dark');
-      themeIcon.src = 'public/moon-icon.svg';
     }
-  } 
+  }
